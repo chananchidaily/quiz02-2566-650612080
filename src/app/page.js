c88c2df9e577a2fb836011dcdb67efa2e46f6404
tonkhao <chananchida_tha@cmu.ac.th>
2023-08-10 14:48:36 +0700

    okkkk

--- src/app/page.js
+++ src/app/page.js
@@ -15,14 +15,14 @@ export default function HomePage() {
         <div className="vstack gap-3">
           <div className="d-flex align-items-center gap-3">
             <img
-              src="/profileImages/handsome.jpg"
+              src="/profileImages/tonkhao.jpg"
               width="48"
               height="48"
               className="rounded-circle"
               style={{ objectFit: "cover" }}
             />
             <span className="fw-semibold fs-5 text-white">
-              Chayanin Suatap 650610560
+              Chananchida Thawornwong 650612080
             </span>
           </div>
 
