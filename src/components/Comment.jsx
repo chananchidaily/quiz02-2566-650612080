5ef845ec40faa818130793add674a779bbf2994a
Tonkhao <chananchida_tha@cmu.ac.th>
2023-09-28 16:43:37 +0700

    ลืมเซฟทำไมถ้าจะลืมคอมมิท

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,17 +5,27 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
-  const numlikess = likeNum > 0 ;
-
   return (
-    <div className="texts">
-      <img src={userImagePath} alt={
-
-
-
-
-      your code here...
-      {/* map-loop render Reply component here */}
+    <div className="d-flex gap-2 my-2">
+      <img
+        src={userImagePath}
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <div
+        className="rounded rounded-3 p-2"
+        style={{ backgroundColor: "#E5E7EB" }}
+      >
+        <span className="fw-semibold">{username}</span>
+        <br />
+        <span>{commentText}</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span className="text-muted">{likeNum}</span>
+        </div>
+      </div>
     </div>
   );
 };
