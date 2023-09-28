819f854c54e663304d6f8266b329df1d22f5ace7
Tonkhao <chananchida_tha@cmu.ac.th>
2023-09-28 17:02:50 +0700

    ทำไมคอมมิทละไท่ขึ้นอ่ะ ทำไมขึ้นอันเดิม

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,115 @@
 "use client";
 
+import { comments } from "@/libs/comments"; 
+import { Comment } from "@/components/Comment"; 
+import { PostOwner } from "@/components/PostOwner";
+import { React } from "react"; 
+
 export default function HomePage() {
   return (
     <div
       style={{ minHeight: "100vh", backgroundColor: "#18191A" }}
       className="p-3"
     >
-      {/* Entire App Container */}
       <div
         style={{ maxWidth: "700px", backgroundColor: "#242526" }}
         className="mx-auto p-3 rounded rounded-3 shadow-sm"
       >
         {/* Post Owner Example*/}
-        <PostOwner
-          userImagePath="/profileImages/tonkhao.jpg"
-          username="Chanancchida Thawornwong 650612080"
-          postText="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207"
-          likeNum="100 คน"
-        />
-
-        {/* map-loop render Comment component here */}
-
-        {comments.map((cmt) => (
-          <Comment
-            userImagePath={cmt.userImagePath}
-            username={cmt.username}
-            replyText={cmt.replyText}
-            likeNum={cmt.likeNum}
-            key={cmt.id}
+        <PostOwner> </PostOwner>
+        {/* Comment Example */}
+        <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/lisa.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
           />
-        ))}
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Lisa
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>999 คน</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Reply Example */}
+        <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src="/profileImages/puppy.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              หมาน้อย
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบ</span>
+            <div className="d-flex align-items-center gap-1">
+            </div>
+          </div>
+        </div>
+        <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src="/profileImages/popcat.png"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Cat Meme
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>ลิซ่าาาาาาา</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>2 คน</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Comment Example */}
+        <div className="d-flex gap-2 my-2">
+          <img
+            src="/profileImages/charliebrown.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              Charlie Brown
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>บ้าไปแล้ว</span>
+            <div className="d-flex align-items-center gap-1">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
